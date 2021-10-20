cb0f143e461645ae39e86600ead05f934dd3756b
GanShishi <87690971+GanShishi@users.noreply.github.com>
2021-10-20 17:26:42 +0900

    Update jquery_自分に合ったセキュリティソフト.js

--- jquery_自分に合ったセキュリティソフト.js
+++ jquery_自分に合ったセキュリティソフト.js
@@ -1,14 +1,14 @@
 $(function(){
 
  $(".result").hide();
- $(".buttons_parent").hide();
+ $(".buttons").hide();
 
  //ボタンがクリックされた時
  $("button").click(function(){
    var qNum = $(".main-question ul li").length;
 
    $(".result").hide();
-   $(".buttons_parent").hide();
+   $(".buttons").hide();
 
    if( $("ul li input:checked").length < qNum ){
      alert("未回答の問題があります");
@@ -23,55 +23,76 @@ $(function(){
 
      if( typeANum >= typeBNum && typeANum >= typeCNum && typeANum >= typeDNum && typeANum >= typeENum && typeANum >= typeFNum && typeWNum == 1) {
        $(".resultA").fadeIn();
+       $(".button_resultA").fadeIn();
      } else if( typeANum >= typeBNum && typeANum >= typeCNum && typeANum >= typeDNum && typeANum >= typeENum && typeANum >= typeFNum && typeWNum != 1) {
        if(typeBNum >= typeCNum && typeBNum >= typeDNum && typeBNum >= typeENum && typeBNum >= typeFNum){
          $(".resultB").fadeIn();
+         $(".button_resultB").fadeIn();
        } else if(typeCNum >= typeBNum && typeCNum >= typeDNum && typeCNum >= typeENum && typeCNum >= typeFNum){
          $(".resultC").fadeIn();
+         $(".button_resultC").fadeIn();
        } else if(typeDNum >= typeBNum && typeDNum >= typeCNum && typeDNum >= typeENum && typeDNum >= typeFNum){
          $(".resultD").fadeIn();
+         $(".button_resultD").fadeIn();
        } else if(typeENum >= typeBNum && typeENum >= typeCNum && typeENum >= typeDNum && typeENum >= typeFNum){
          $(".resultE").fadeIn();
+         $(".button_resultE").fadeIn();
        } else if(typeFNum >= typeBNum && typeFNum >= typeCNum && typeFNum >= typeDNum && typeFNum >= typeENum){
          $(".resultF").fadeIn();
+         $(".button_resultF").fadeIn();
        }
      } else if( typeBNum >= typeANum && typeBNum >= typeCNum && typeBNum >= typeDNum && typeBNum >= typeENum && typeBNum >= typeFNum && typeWNum == 1) {
        $(".resultB").fadeIn();
+       $(".button_resultB").fadeIn();
      } else if( typeBNum >= typeANum && typeBNum >= typeCNum && typeBNum >= typeDNum && typeBNum >= typeENum && typeBNum >= typeFNum && typeWNum != 1) {
        if(typeANum >= typeCNum && typeANum >= typeDNum && typeANum >= typeENum && typeANum >= typeFNum){
          $(".resultA").fadeIn();
+         $(".button_resultA").fadeIn();
        } else if(typeCNum >= typeANum && typeCNum >= typeDNum && typeCNum >= typeENum && typeCNum >= typeFNum){
          $(".resultC").fadeIn();
+         $(".button_resultC").fadeIn();
        } else if(typeDNum >= typeANum && typeDNum >= typeCNum && typeDNum >= typeENum && typeDNum >= typeFNum) {
          $(".resultD").fadeIn();
+         $(".button_resultD").fadeIn();
        } else if(typeENum >= typeANum && typeENum >= typeCNum && typeENum >= typeDNum && typeENum >= typeFNum) {
          $(".resultE").fadeIn();
+         $(".button_resultE").fadeIn();
        } else if(typeFNum >= typeANum && typeFNum >= typeCNum && typeFNum >= typeDNum && typeFNum >= typeENum) {
          $(".resultF").fadeIn();
+         $(".button_resultF").fadeIn();
        }
      } else if( typeCNum >= typeANum && typeCNum >= typeBNum && typeCNum >= typeDNum && typeCNum >= typeENum && typeCNum >= typeFNum) {
        $(".resultC").fadeIn();
+       $(".button_resultC").fadeIn();
      } else if( typeDNum >= typeANum && typeDNum >= typeBNum && typeDNum >= typeCNum && typeDNum >= typeENum && typeDNum >= typeFNum && typeWNum == 1) {
        $(".resultD").fadeIn();
+       $(".button_resultD").fadeIn();
      } else if( typeDNum >= typeANum && typeDNum >= typeBNum && typeDNum >= typeCNum && typeDNum >= typeENum && typeDNum >= typeFNum && typeWNum != 1) {
        if(typeANum >= typeBNum && typeANum >= typeCNum && typeANum >= typeENum && typeANum >= typeFNum){
          $(".resultA").fadeIn();
+         $(".button_resultA").fadeIn();
        } else if(typeBNum >= typeANum && typeBNum >= typeCNum && typeBNum >= typeENum && typeBNum >= typeFNum){
          $(".resultB").fadeIn();
+         $(".button_resultB").fadeIn();
        } else if(typeCNum >= typeANum && typeCNum >= typeBNum && typeCNum >= typeENum && typeCNum >= typeFNum){
          $(".resultC").fadeIn();
+         $(".button_resultC").fadeIn();
        } else if(typeENum >= typeANum && typeENum >= typeBNum && typeENum >= typeCNum && typeENum >= typeFNum){
          $(".resultE").fadeIn();
+         $(".button_resultE").fadeIn();
        } else if(typeFNum >= typeANum && typeFNum >= typeBNum && typeFNum >= typeCNum && typeFNum >= typeENum){
          $(".resultF").fadeIn();
+         $(".button_resultF").fadeIn();
        }
      } else if( typeENum >= typeANum && typeENum >= typeBNum && typeENum >= typeCNum && typeENum >= typeDNum && typeENum >= typeFNum) {
        $(".resultE").fadeIn();
+       $(".button_resultE").fadeIn();
      } else if( typeFNum >= typeANum && typeFNum >= typeBNum && typeFNum >= typeCNum && typeFNum >= typeDNum && typeFNum >= typeENum) {
        $(".resultF").fadeIn();
+       $(".button_resultF").fadeIn();
      }
 
-     $(".buttons_parent").fadeIn();
+     $(".button_ToMain").fadeIn();
 
    }
  });
